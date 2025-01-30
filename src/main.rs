f1b0be792692aa745c1572b5b9f9654868e3df99
Bart Massey <bart.massey@gmail.com>
2025-01-30 12:29:24 -0800

    exhibited thiserror and anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+extern crate anyhow;
+extern crate thiserror;
+
 fn _min(a: &[u8]) -> Option<u8> {
     if a.is_empty() {
         return None;
@@ -33,26 +36,32 @@ fn _option() {
 
 // pub fn open<P: AsRef<Path>>(path: P) -> Result<File, std::io::Error>
 
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum FirstError {
+/*
+    #[error("error opening file: {0}")]
     OpenError(std::io::Error),
+    #[error("error reading file: {0}")]
     ReadError(std::io::Error),
+*/
+    #[error("non-ascii first character")]
     NonAscii,
+    #[error("empty file")]
     EmptyFile,
 }
 
-fn first_char_of_file(path: &str) -> Result<char, FirstError> {
+fn first_char_of_file(path: &str) -> Result<char, anyhow::Error> {
     use std::io::Read;
 
-    let mut f = std::fs::File::open(path).map_err(FirstError::OpenError)?;
+    let mut f = std::fs::File::open(path)?;
     let mut c = [0u8];
 
-    match f.read(&mut c).map_err(FirstError::ReadError)? {
+    match f.read(&mut c)? {
         1 => match c[0] {
-            v if v <= 0x7f => Ok(v as char),
-            _ => Err(FirstError::NonAscii),
+            v if v.is_ascii() => Ok(v as char),
+            _ => Err(FirstError::NonAscii.into()),
         }
-        0 => Err(FirstError::EmptyFile),
+        0 => Err(FirstError::EmptyFile.into()),
         n => panic!("internal error: overread: {n}"),
     }
 }
